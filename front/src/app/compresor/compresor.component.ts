6449c09710935f6dc9886fadce6bcaeb6ad099c0
Andres Gomez <andres.e.gomez@gmail.com>
2019-04-21 23:14:03 -0300

    add mensaje error

--- front/src/app/compresor/compresor.component.ts
+++ front/src/app/compresor/compresor.component.ts
@@ -4,8 +4,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'},{'Content-Type':'application/json'},{'Access-Control-Allow-Origin':'*'},{'Access-Control-Allow-Headers':'Content-Type'}),
-	observe: 'body',
+  headers: new HttpHeaders(
+  {'Content-Type': 'application/json'},
+  {'Content-Type':'application/json'},{'Access-Control-Allow-Origin':'*'},
+  {'Access-Control-Allow-Headers':'Content-Type'}
+  ),
+	observe: 'response',
 	responseType: 'json'
 }
 
@@ -32,13 +36,16 @@ this.httpClient.post("https://compressor-237218.appspot.com/compress",
 data  => {
 this.compressed = data.compressed;
 },
-error  => {
-console.log('ERROR RECIBIDO: ',error);
-this.compressed = error.mensaje;
+(err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            //A client-side or network error occurred.				 
+            console.log('An error occurred:', err.error.message);
+          } else {			 
+            console.log('Backend returned status code: ', err.status);
+           this.compressed = err.error.mensaje;
+          }
+        }
+     );
 }
-);
-}
-
-
 
 }
